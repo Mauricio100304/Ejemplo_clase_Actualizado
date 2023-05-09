b5b35929cfcbe843aa6719ed73c6d3002a2a8769
mauricio100304 <maurogrimm10@gmail.com>
2023-05-08 23:30:20 -0500

    1

---
+++ app/src/main/java/com/example/rodriguez_celis/FormularioActivity.java
@@ -0,0 +1,36 @@
+package com.example.rodriguez_celis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    private EditText etNombre, etPrecio, etImagen;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+        etNombre=findViewById(R.id.et_nombre_formulario);
+        etPrecio=findViewById(R.id.et_precio_formulario);
+        etImagen=findViewById(R.id.et_imagen_formulario);
+    }
+    public void clickGuardar(View view){
+        String nombre = etNombre.getText().toString();
+        Double precio = Double.parseDouble(etPrecio.getText().toString());
+        String url = etImagen.getText().toString();
+        Producto nuevoProducto = new Producto();
+        nuevoProducto.setNombre(nombre);
+        nuevoProducto.setPrecio(precio);
+        nuevoProducto.setUrImagen(url);
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        firestore.collection("productos").add(nuevoProducto);
+        Toast.makeText(this,"Se creó el producto", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/rodriguez_celis/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.rodriguez_celis;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.SharedElementCallback;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+        private ArrayList<Producto> listaPrincipalProductos = new ArrayList<>();
+        private RecyclerView rvListadoProductos;
+        private AdaptadorPersonalizado miAdaptador;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+                setTitle(getString(R.string.txt_listado_productos));
+
+                rvListadoProductos = findViewById(R.id.rv_listado_productos);
+
+                miAdaptador = new AdaptadorPersonalizado(listaPrincipalProductos);
+                miAdaptador.setOnItemClickListener(new AdaptadorPersonalizado.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(Producto miProducto, int posicion) {
+
+                                Intent intent = new Intent(MainActivity.this, DetalleActivity.class);
+                                intent.putExtra("producto",miProducto);
+                                startActivity(intent);
+
+                        }
+
+                        @Override
+                        public void onItemBtnEliminarClick(Producto miProducto, int posicion) {
+                                FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+                                firestore.collection("productos").document(miProducto.getId()).delete();
+                                listaPrincipalProductos.remove(posicion);
+                                miAdaptador.setListadoInformacion(listaPrincipalProductos);
+                        }
+                });
+
+                rvListadoProductos.setAdapter(miAdaptador);
+                rvListadoProductos.setLayoutManager(new LinearLayoutManager(this));
+
+        }
+
+        @Override
+        protected void onResume() {
+                super.onResume();
+                listaPrincipalProductos.clear();
+                cargarDatos();
+        }
+
+        public void cargarDatos(){
+                FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+                firestore.collection("productos").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if(task.isSuccessful()){
+                                        for(DocumentSnapshot document : task.getResult()){
+                                                Producto productoAtrapado = document.toObject(Producto.class);
+                                                productoAtrapado.setId(document.getId());
+                                                listaPrincipalProductos.add(productoAtrapado);
+                                        }
+                                        miAdaptador.setListadoInformacion(listaPrincipalProductos);
+                                }else{
+                                        Toast.makeText(MainActivity.this, "No se pudo conectar al servidor", Toast.LENGTH_SHORT).show();
+                                }
+                        }
+                });
+
+        }
+        public void clickCerrarSesion(View view){
+                SharedPreferences miPreferencias = getSharedPreferences("Tienda_App",MODE_PRIVATE);
+                SharedPreferences.Editor myEditor = miPreferencias.edit();
+                myEditor.clear();
+                myEditor.apply();
+
+                startActivity(new Intent(this,LoginActivity.class));
+                finish();
+        }
+        public void clickAgregarProducto(View view){
+                startActivity(new Intent(this,FormularioActivity.class));
+        }
+}
